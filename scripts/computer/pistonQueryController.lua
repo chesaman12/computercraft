38f6b011960dc18ac7af9a8d8bf51d92674cd1bc
chesaman12 <chesaman12@gmail.com>
2022-03-13 03:50:14 -0700

    Cleaning up spelling.. doh

--- scripts/computer/pistonQueryController.lua
+++ scripts/computer/pistonQueryController.lua
@@ -2,19 +2,19 @@
 -- http://www.computercraft.info/forums2/index.php?/topic/3088-how-to-guide-gps-global-position-system/
 
 -- add computers ontop of create blocks that require a redstone
--- and will be listening for signal transimition
+-- and will be listening for signal transmission
 -- IE gearshift, pulley, clutch
 
--- add a wireless modem to call computers
+-- add a wireless modem to all computers
 
 -- using the gps system, add a computer (client connection) to the
 -- query that will act as a broadcaster and sends messages
--- based in its z coord (as a startup script, with infinte looping)
+-- based in its z coord (as a startup script, with infinite  looping)
 -- See notes on gps and the rednet API
 -- https://tweaked.cc/module/rednet.html
 
--- for computers that are listening for a signal transimition,
--- excute function upon recieving a specific message / action
+-- for computers that are listening for a signal transmission,
+-- execute function upon receiving a specific message / action
 
 -- messages / action types
 --      LOWER_QUERY
@@ -23,20 +23,20 @@
 --      PAUSE_JOB
 --      RESUME_JOB
 
--- using the actions types from the broadcaaster,
+-- using the actions types from the broadcaster,
 -- related metadata / payload can be included
 
 -- broadcaster metadata / payload event structure
 -- {actionType: string, payload: table}
 
--- based on action/event, computer will emmit a redstone signal below / beside
+-- based on action/event, computer will emit a redstone signal below / beside
 -- it based on start configuration
 
 -- note: we can monitor a buffer chest with open OpenPeripheral to see
 -- if we should pause the query (IE, storage system is full)
 
 -- OTHER
--- add a chunk loader to the querry
+-- add a chunk loader to the query
 -- add a console log with giant text indicating what the computer action is
     -- the computer above the clutch will be sending pause/resume
     -- so we can display something to indicate this with possible logging
